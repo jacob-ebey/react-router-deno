868bc8e43bf0b984546ef793caad21bf3b87fd1c
Jacob Ebey <jacob.ebey@live.com>
2025-05-26 14:52:12 -0700

    initial commit

---
+++ server.ts
@@ -0,0 +1,24 @@
+import { serveDir } from "@std/http/file-server";
+import type { ServerBuild } from "react-router";
+import { createRequestHandler } from "react-router";
+
+const handler = createRequestHandler(
+  () => import("./build/server/index.js") as Promise<ServerBuild>,
+  "production",
+);
+
+Deno.serve((request: Request): Promise<Response> => {
+  const pathname = new URL(request.url).pathname;
+
+  if (pathname.startsWith("/assets/")) {
+    return serveDir(request, {
+      fsRoot: "build/client/assets",
+      urlRoot: "assets",
+      headers: [
+        "Cache-Control: public, max-age=31536000, immutable",
+      ],
+    });
+  }
+
+  return handler(request);
+});
